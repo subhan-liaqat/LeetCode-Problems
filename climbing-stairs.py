260f5f3026d2f77119cb2090bcb50ffcfcb2924a
Subhan Liaqat <77165727+subhan-liaqat@users.noreply.github.com>
2024-02-06 01:30:32 +0500

    Create climbing-stairs.py

---
+++ climbing-stairs.py
@@ -0,0 +1,14 @@
+class Solution:
+
+    cache = {}
+    def climbStairs(self, n: int) -> int:
+        if n in self.cache:
+            return self.cache[n]
+        if n == 1:
+            val = 1
+        elif n == 2:
+            val = 2
+        elif n > 2:
+            val = self.climbStairs(n-1)+self.climbStairs(n-2)
+        self.cache[n] = val
+        return val
