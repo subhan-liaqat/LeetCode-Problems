32999a602291cb86c83d04197471aa3be93b0ec1
Subhan Liaqat <77165727+subhan-liaqat@users.noreply.github.com>
2024-02-07 00:43:11 +0500

    Create substrings-of-size-three-with-distinct-characters.py

---
+++ substrings-of-size-three-with-distinct-characters.py
@@ -0,0 +1,8 @@
+class Solution:
+    def countGoodSubstrings(self, s: str) -> int:
+        count=0
+        for i in range(len(s)-2):
+            window=s[i:i+3]
+            if len(set(window))==3:
+                count+=1
+        return count
