f0e9b248289c5451bc919b20fd5c800a5b26b4b3
Subhan Liaqat <77165727+subhan-liaqat@users.noreply.github.com>
2024-02-23 21:32:54 +0500

    Update 1700-number-of-students-unable-to-eat-lunch.py

--- 1700-number-of-students-unable-to-eat-lunch.py
+++ 1700-number-of-students-unable-to-eat-lunch.py
@@ -1,1 +1,20 @@
+class Solution:
+    def countStudents(self, students: List[int], sandwiches: List[int]) -> int:
+        # circular sandwich = 0
+        # square sandwich =  1
 
+        count = len(students)
+        while sandwiches and students and sandwiches[0] in students:
+
+            # if the first student does not like the upper sandwich
+            if sandwiches[0] != students[0]:
+                students.append(students[0])
+                students.pop(0)
+
+            # if the first student like the upper sandwich
+            else:
+                students.pop(0)
+                sandwiches.pop(0)
+                count -= 1
+
+        return count
