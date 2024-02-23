937f5c9624d724d0498ba04f2a4d32ad785540f8
Subhan Liaqat <77165727+subhan-liaqat@users.noreply.github.com>
2024-02-23 23:41:17 +0500

    Update 66-plus-one.py

--- 66-plus-one.py
+++ 66-plus-one.py
@@ -1,3 +1,4 @@
+# Approach 1
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
         for i in range(len(digits)-1, -1, -1):
@@ -7,3 +8,24 @@ class Solution:
                 digits[i] = digits[i] + 1
                 return digits
         return [1] + digits
+
+
+# Approach 2
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+
+        # Convert array into string
+        res = ""
+        for i in digits:
+            res += str(i)
+
+        # Convert string into integer to add once and then again convert to string
+        res = int(res) + 1
+        res = str(res)
+
+        # Convert string into array
+        arr = []
+        for i in res:
+            arr+=[int(i)]
+
+        return arr
