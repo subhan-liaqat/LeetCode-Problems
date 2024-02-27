93c525f4491175e9d541a4abec837fd466e0279d
Subhan Liaqat <77165727+subhan-liaqat@users.noreply.github.com>
2024-02-27 17:10:48 +0500

    Update 151-reverse-words-in-a-string.py

--- 151-reverse-words-in-a-string.py
+++ 151-reverse-words-in-a-string.py
@@ -1,1 +1,9 @@
-
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        s = s.split()
+        ans = ''
+        
+        for i in range(len(s)-1, -1, -1):
+            ans += s[i]
+            ans += ' '
+        return ans[:-1]
