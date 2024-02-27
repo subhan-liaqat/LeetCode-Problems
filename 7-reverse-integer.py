6b19f5c97878820ef3e88bc74e02a778e44f80b3
Subhan Liaqat <77165727+subhan-liaqat@users.noreply.github.com>
2024-02-27 17:08:51 +0500

    Update 7-reverse-integer.py

--- 7-reverse-integer.py
+++ 7-reverse-integer.py
@@ -1,1 +1,23 @@
-
+class Solution:
+    def reverse(self, x: int) -> int:
+        x = str(x)
+        y = ""
+        
+        if x[0] == '+' or x[0] == '-':
+            if x[0] == '+':
+                y += '+'
+            else:
+                y += '-'
+                
+            for i in range(len(x)-1, 0, -1):
+                y += x[i]
+                
+        else:
+            for i in range(len(x)-1, -1, -1):
+                y += x[i]
+                
+        y = int(y)
+        if y>2**31-1 or y<-2**31:
+            return 0
+        
+        return y
