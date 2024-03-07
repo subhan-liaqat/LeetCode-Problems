70cb33045a2f30271dc08d53d1eb0d25cb70fb73
Subhan Liaqat <77165727+subhan-liaqat@users.noreply.github.com>
2024-03-07 17:02:09 +0500

    Update 771-jewels-and-stones.py

--- 771-jewels-and-stones.py
+++ 771-jewels-and-stones.py
@@ -1,1 +1,28 @@
+# Approach 1
+class Solution:
+    def numJewelsInStones(self, jewels: str, stones: str) -> int:
+        stoneBox = {}
+        noOfJewels = 0
 
+        for s in stones:
+            if s not in stoneBox:
+                stoneBox[s] = 1
+            else:
+                stoneBox[s] += 1
+
+        for j in jewels:
+            if j in stoneBox:
+                noOfJewels += stoneBox[j]
+
+        return noOfJewels
+
+
+  # Approach 2
+  class Solution:
+    def numJewelsInStones(self, jewels: str, stones: str) -> int:
+        count = 0
+        for i in stones:
+            for j in jewels:
+                if i == j:
+                    count += 1
+        return count
