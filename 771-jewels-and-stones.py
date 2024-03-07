4b49a33cc1f1b89cf47a39592262b37c8a6897d2
Subhan Liaqat <77165727+subhan-liaqat@users.noreply.github.com>
2024-03-07 17:11:53 +0500

    Update 771-jewels-and-stones.py

--- 771-jewels-and-stones.py
+++ 771-jewels-and-stones.py
@@ -29,3 +29,10 @@ class Solution:
 
         
 # Approach 3
+class Solution:
+    def numJewelsInStones(self, jewels: str, stones: str) -> int:
+        count = 0
+        for i in stones:
+            if i in jewels:
+                count += 1
+        return count
