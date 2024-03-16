0861d8602d61ccfc41baa2908217b0ce03dc6962
Subhan Liaqat <77165727+subhan-liaqat@users.noreply.github.com>
2024-03-16 15:30:32 +0500

    Update 1984-minimum-difference-between-highest-and-lowest-of-k-scores.py

--- 1984-minimum-difference-between-highest-and-lowest-of-k-scores/1984-minimum-difference-between-highest-and-lowest-of-k-scores.py
+++ 1984-minimum-difference-between-highest-and-lowest-of-k-scores/1984-minimum-difference-between-highest-and-lowest-of-k-scores.py
@@ -1,3 +1,4 @@
+# Approach 1
 class Solution:
     def minimumDifference(self, nums: List[int], k: int) -> int:
 
@@ -12,4 +13,19 @@ class Solution:
                 high = nums[i]
                 minDiff = min(minDiff, high - low)
 
+        return minDiff
+
+
+# Approach 2
+class Solution:
+    def minimumDifference(self, nums: List[int], k: int) -> int:
+        nums.sort()  # nums = [1, 4, 7, 9]
+        l = 0
+        r = k - 1
+
+        minDiff = float('inf')
+        while r < len(nums):  # 3 < 4
+            minDiff = min(minDiff, nums[r] - nums[l])
+            l += 1
+            r += 1
         return minDiff
