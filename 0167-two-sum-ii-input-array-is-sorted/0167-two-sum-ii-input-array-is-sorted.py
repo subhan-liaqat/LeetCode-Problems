11cdd58cf52b513cd201d3cd5ae7798b291dd052
Subhan Liaqat <77165727+subhan-liaqat@users.noreply.github.com>
2024-03-16 15:36:27 +0500

    Update 0167-two-sum-ii-input-array-is-sorted.py

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
@@ -1,3 +1,4 @@
+# Approach 1
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
         l = 0
@@ -12,3 +13,20 @@ class Solution:
                 l += 1
             else:
                 return [l+1, r+1]
+
+
+# Approach 1 (modified)
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        l = 0
+        r = len(numbers) - 1
+
+        while l <= r:
+            sum = numbers[l] + numbers[r]
+
+            if sum == target:
+                return [l+1, r+1]
+            elif sum > target:
+                r -= 1
+            elif sum < target:
+                l += 1
