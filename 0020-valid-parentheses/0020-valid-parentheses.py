c9529bd753282f27416b930ca0c1d4aa6da22979
Subhan Liaqat <77165727+subhan-liaqat@users.noreply.github.com>
2024-03-17 14:32:48 +0500

    Update 0020-valid-parentheses.py

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -8,13 +8,10 @@ class Solution:
         stack = []
 
         for i in s:
-
             if i in brackets: # it's opening bracket
                 stack.append(i)
-
             else: # it's closing bracket
                 if not stack: return False
-
                 b = stack.pop()
                 if i != brackets[b]: return False
 
