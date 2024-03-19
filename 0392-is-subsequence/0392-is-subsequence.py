e043395f43da57135f144499b1d1e31302bd1df7
Subhan Liaqat <77165727+subhan-liaqat@users.noreply.github.com>
2024-03-19 17:07:38 +0500

    Update 0392-is-subsequence.py

--- 0392-is-subsequence/0392-is-subsequence.py
+++ 0392-is-subsequence/0392-is-subsequence.py
@@ -1,3 +1,25 @@
+# Approach 1
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        if s == '':
+            return True
+
+        left = 0
+        right = 0
+
+        while left < len(s) and right < len(t):
+
+            if s[left] == t[right]:
+                left += 1
+
+                if left == len(s):
+                    return True
+            right += 1
+            
+        return False
+
+
+# Approach 2
 class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
 
