2960a1fa7045c2ba4d791e24af509feff989eb1b
Subhan Liaqat <77165727+subhan-liaqat@users.noreply.github.com>
2024-03-30 05:21:36 +0500

    Update 682-baseball-game.py

--- 682-baseball-game.py
+++ 682-baseball-game.py
@@ -1,1 +1,20 @@
-
+class Solution:
+    def calPoints(self, ops: List[str]) -> int:
+        stack = [] 
+        # stack = empty
+        # operation = D
+        for operation in ops:
+          if operation == '+':
+            # to sum the previous values
+            stack.append(stack[-1]+stack[-2])
+          
+          elif operation == 'C':
+            stack.pop()
+          
+          elif operation == 'D':
+            stack.append(2 * stack[-1] )
+          
+          else:
+            stack.append(int(operation))
+        
+        return sum(stack)
