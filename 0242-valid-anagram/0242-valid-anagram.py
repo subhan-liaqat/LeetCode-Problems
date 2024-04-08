8ec52bea06b4d0455de2ec2fa83c019c1e2f203c
Subhan Liaqat <77165727+subhan-liaqat@users.noreply.github.com>
2024-04-08 10:11:42 +0500

    Update 0242-valid-anagram.py

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,3 +1,28 @@
+# Approach 1
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+
+        countS, countT = {}, {}
+
+        for i in range(len(s)):
+            countS[s[i]] = 1 + countS.get(s[i], 0)
+            countT[t[i]] = 1 + countT.get(t[i], 0)
+        
+        for j in countS:
+            if countS[j] != countT.get(j):
+                return False
+        return True
+
+
+# Approach 2
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         return Counter(s) == Counter(t)
+
+
+# Approach 3
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        return sorted(s) == sorted(t)
