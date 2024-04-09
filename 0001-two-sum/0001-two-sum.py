50ade965c26499dd8c76ce4111e6213223bd36d4
Subhan Liaqat <77165727+subhan-liaqat@users.noreply.github.com>
2024-04-09 09:51:31 +0500

    Update 0001-two-sum.py

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,8 +1,11 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        dict={}
-        for i,n in enumerate(nums):
-            if n in dict:
-                return dict[n],i
-            else:
-                dict[target-n]=i
+        hashMap = {}
+
+        for i, n in enumerate(nums):
+
+            diff = target - n
+            if diff in hashMap:
+                return [hashMap[diff], i]
+
+            hashMap[n] = i
