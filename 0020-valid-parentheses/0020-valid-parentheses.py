a69c524df023ea0fbe6e05fbd210cdaa7b4da279
Subhan Liaqat <77165727+subhan-liaqat@users.noreply.github.com>
2025-05-16 08:27:15 +0500

    Time: 26 ms (4.32%), Space: 16.6 MB (100%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -8,10 +8,13 @@ class Solution:
         stack = []
 
         for i in s:
+
             if i in brackets: # it's opening bracket
                 stack.append(i)
+
             else: # it's closing bracket
                 if not stack: return False
+
                 b = stack.pop()
                 if i != brackets[b]: return False
 
